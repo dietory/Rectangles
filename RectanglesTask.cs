5945eee84b2f8bfab3c43fa4d3083a12e34c0012
Robert I <robertorbbit@gmail.com>
2021-08-08 12:48:28 +0500

    Добавить решение задачи

--- RectanglesTask.cs
+++ RectanglesTask.cs
@@ -1,20 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Rectangles
 {
+	public class Point
+    {
+		public int x;
+		public int y;
+    }
 	public static class RectanglesTask
 	{
 		// Пересекаются ли два прямоугольника (пересечение только по границе также считается пересечением)
 		public static bool AreIntersected(Rectangle r1, Rectangle r2)
 		{
-			// так можно обратиться к координатам левого верхнего угла первого прямоугольника: r1.Left, r1.Top
-			return true;
+			bool OX = ((r2.Left >= r1.Left && r2.Left <= r1.Right) || (r2.Right >= r1.Left && r2.Right <= r1.Right)) || ((r1.Left >= r2.Left && r1.Left <= r2.Right) || (r1.Right >= r2.Left && r1.Right <= r2.Right));
+			bool OY = ((r2.Top >= r1.Top && r2.Top <= r1.Bottom) || (r2.Bottom >= r1.Top && r2.Bottom <= r1.Bottom)) || ((r1.Top >= r2.Top && r1.Top <= r2.Bottom) || (r1.Bottom >= r2.Top && r1.Bottom <= r2.Bottom));
+
+			return OX && OY;
 		}
 
 		// Площадь пересечения прямоугольников
 		public static int IntersectionSquare(Rectangle r1, Rectangle r2)
 		{
-			return 0;
+			int oxLength = 0;
+			int oyLength = 0;
+			if (r1.Left <= r2.Left && r1.Right >= r2.Right)
+            {
+				oxLength = r2.Right - r2.Left;
+			}
+			else if (r2.Left <= r1.Left && r2.Right >= r1.Right)
+			{
+				oxLength = r1.Right - r1.Left;
+			}
+			else if ((r2.Left >= r1.Left && r2.Left <= r1.Right) || (r1.Right >= r2.Left && r1.Right <= r2.Right))
+            {
+				oxLength = (r2.Right - r1.Left) - ((r2.Left - r1.Left)+(r2.Right - r1.Right));
+            }
+			else if ((r2.Right >= r1.Left && r2.Right <= r1.Right) || (r1.Left >= r2.Left && r1.Left <= r2.Right))
+            {
+				oxLength = (r1.Right - r2.Left) - ((r1.Left - r2.Left) + (r1.Right - r2.Right));
+			}
+
+			if (r1.Top <= r2.Top && r1.Bottom >= r2.Bottom)
+			{
+				oyLength = r2.Bottom - r2.Top;
+			}
+			else if (r2.Top <= r1.Top && r2.Bottom >= r1.Bottom)
+			{
+				oyLength = r1.Bottom - r1.Top;
+			}
+			else if ((r2.Top >= r1.Top && r2.Top <= r1.Bottom) || (r1.Bottom >= r2.Top && r1.Bottom <= r2.Bottom))
+			{
+				oyLength = (r2.Bottom - r1.Top) - ((r2.Top - r1.Top) + (r2.Bottom - r1.Bottom));
+			}
+			else if ((r2.Bottom >= r1.Top && r2.Bottom <= r1.Bottom) || (r1.Top >= r2.Top && r1.Top <= r2.Bottom))
+			{
+				oyLength = (r1.Bottom - r2.Top) - ((r1.Top - r2.Top) + (r1.Bottom - r2.Bottom));
+			}
+
+			return Math.Abs(oxLength) * Math.Abs(oyLength);
 		}
 
 		// Если один из прямоугольников целиком находится внутри другого — вернуть номер (с нуля) внутреннего.
@@ -22,7 +66,12 @@ namespace Rectangles
 		// Если прямоугольники совпадают, можно вернуть номер любого из них.
 		public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
 		{
-			return -1;
+			if (r1.Left <= r2.Left && r1.Right >= r2.Right && r1.Top <= r2.Top && r1.Bottom >= r2.Bottom)
+				return 1;
+			else if (r2.Left <= r1.Left && r2.Right >= r1.Right && r2.Top <= r1.Top && r2.Bottom >= r1.Bottom)
+				return 0;
+			else
+				return -1;
 		}
 	}
 }
